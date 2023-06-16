8fd463cf374ba146b7fc2c576248ef449b5b4a4c
112batuhan <batuhancakir97@hotmail.com>
2023-06-16 21:59:34 +0300

    frontend-adding date input

--- frontend/src/routes/email_import/+page.svelte
+++ frontend/src/routes/email_import/+page.svelte
@@ -2,28 +2,35 @@
 	// @ts-nocheck
 
 	let email_body = '';
+	let send_date = '';
 
 	async function insert_email() {
+		console.log(send_date);
 		const res = await fetch('http://127.0.0.1:3040/insert_email', {
-			method: 'post',
+			method: 'POST',
 			credentials: 'include',
-			body: JSON.stringify({email:email_body})
+			body: JSON.stringify({ email: email_body, date: send_date })
 		});
 		if (res.ok) {
-			
 		}
 	}
 </script>
 
 <textarea class="email_input" bind:value={email_body} />
+
 <div>
-	<button on:click={insert_email} >Submit</button>
+	<input bind:value={send_date} type="date" />
+	select a date to activate button
 </div>
+{#if send_date !== ''}
+<div>
+	<button on:click={insert_email}>Submit</button>
+</div>
+{/if}
 
 <style>
 	.email_input {
 		height: 600px;
 		width: 600px;
 	}
-
 </style>
