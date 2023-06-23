2fb62bc3d77f5e1323fe7c4ff497c34b837a2316
112batuhan <batuhancakir97@hotmail.com>
2023-06-23 17:13:50 +0300

    frontend - email persistent pagination

--- frontend/src/routes/emails/+page.svelte
+++ frontend/src/routes/emails/+page.svelte
@@ -2,7 +2,7 @@
 	import { Accordion, AccordionItem, Paginator } from '@skeletonlabs/skeleton';
 	import { onMount } from 'svelte';
 
-	let emails: string | any[] = [];
+	let emails: any = [];
 
 	onMount(async () => {
 		try {
@@ -15,6 +15,10 @@
 				emails = await res.json();
 				// Paginator email length update
 				page.size = emails.length;
+
+				emails.forEach((item: any, i: number) => {
+					item.display_id = i;
+				});
 			} else {
 				emails = [];
 			}
@@ -35,29 +39,38 @@
 		page.offset * page.limit,
 		page.offset * page.limit + page.limit
 	);
+
+	let active_item = -1;
+
+	function calculateLastActivePage(amount_event: any) {
+		page.offset = Math.floor(active_item / amount_event.detail);
+	}
 </script>
 
 <div class="card p-4 w-[100%] min-w-[600px]">
 	{#if emails.length > 0}
 		<Accordion autocollapse>
 			{#each paginatedEmails as email}
-				<AccordionItem class="variant-ghost-surface rounded-md">
+				<AccordionItem
+					on:toggle={() => (active_item = email.display_id)}
+					class="variant-ghost-surface rounded-md"
+				>
 					<svelte:fragment slot="lead">&#9993;&#65039;</svelte:fragment>
 					<svelte:fragment slot="summary">
-						<div class="variant-ghost-surface p-1 grid rounded-2xl grid-cols-6 gap-4 items-center">
-							<div class="pl-10 col-span-3"><strong>{email.subject}</strong></div>
-							<div class="justify-self-center">Date To Send: {email.send_date}</div>
-							<div class="justify-self-center">{email.is_html ? 'HTML' : 'TEXT'}</div>
-							<div class="justify-self-end pr-3 flex flex-wrap justify-center">
-								<button class="btn variant-filled-primary p-1 m-1">Edit</button>
-								<button class="btn variant-filled-primary p-1 m-1">Send</button>
-								<button class="btn variant-filled-primary p-1 m-1">Hide</button>
-							</div>
+						<div class="variant-ghost-surface p-1 grid rounded-2xl grid-cols-12 gap-4 items-center">
+							<div class="pl-10 col-span-8"><strong>{email.subject}</strong></div>
+							<div class="justify-self-center text-center">Send Date {email.send_date}</div>
+							<div class="justify-self-center text-center">{email.is_html ? 'HTML' : 'TEXT'}</div>
+							{#if active_item == email.display_id}
+								<div class="justify-self-center pr-3 col-span-2 flex flex-wrap justify-center">
+									<button class="btn variant-filled-primary p-1 m-1">Edit</button>
+									<button class="btn variant-filled-primary p-1 m-1">Send</button>
+									<button class="btn variant-filled-primary p-1 m-1">Hide</button>
+								</div>
+							{/if}
 						</div>
 					</svelte:fragment>
 					<svelte:fragment slot="content">
-						<!-- ... -->
-
 						<div
 							style="border-radius:0.3rem; background-color:#c2a6f5; 100%; height: 350px; overflow: hidden;"
 						>
@@ -75,5 +88,11 @@
 		<p>No emails found.</p>
 	{/if}
 
-	<Paginator class="m-3" bind:settings={page} showNumerals maxNumerals={1} />
+	<Paginator
+		class="m-3"
+		bind:settings={page}
+		on:amount={calculateLastActivePage}
+		showNumerals
+		maxNumerals={1}
+	/>
 </div>
