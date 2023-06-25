f6f596960b60e3dae1c210a9aa23eb5171d2c7b3
112batuhan <batuhancakir97@hotmail.com>
2023-06-26 00:05:16 +0300

    tower_governor crate

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -9,10 +9,16 @@ use std::time::Duration;
 use api::authentication::{auto_login, check_session_token, login, logout, sign_up};
 use api::emails::{create_email, get_emails, send_demo_email};
 use api::SharedState;
+use axum::error_handling::HandleErrorLayer;
 use axum::http::{header, Method};
 use axum::routing::{delete, get, post};
-use axum::{middleware, Router};
+use axum::{middleware, BoxError, Router};
 use dotenv::dotenv;
+use tower::ServiceBuilder;
+use tower_governor::errors::display_error;
+use tower_governor::governor::GovernorConfigBuilder;
+use tower_governor::key_extractor::SmartIpKeyExtractor;
+use tower_governor::GovernorLayer;
 use tower_http::cors::CorsLayer;
 
 #[tokio::main]
@@ -36,6 +42,16 @@ async fn main() {
         .allow_credentials(true)
         .max_age(Duration::from_secs(3600));
 
+    let governor_conf = Box::new(
+        GovernorConfigBuilder::default()
+            .per_second(20)
+            .burst_size(5)
+            .key_extractor(SmartIpKeyExtractor)
+            .use_headers()
+            .finish()
+            .unwrap(),
+    );
+
     let app = Router::new()
         .route("/send_email/:email_id", get(send_demo_email))
         .route("/auto_login", get(auto_login))
@@ -48,6 +64,16 @@ async fn main() {
         .route("/logout", delete(logout))
         .route("/sign_up", post(sign_up))
         .route("/login", post(login))
+        .layer(
+            ServiceBuilder::new()
+                .layer(HandleErrorLayer::new(|e: BoxError| async move {
+                    display_error(e)
+                }))
+                .layer(GovernorLayer {
+                    // We can leak this because it is created once and then
+                    config: Box::leak(governor_conf),
+                }),
+        )
         .layer(cors)
         .with_state(state);
 
@@ -56,7 +82,7 @@ async fn main() {
         std::env::var("PORT").unwrap().parse::<u16>().unwrap(),
     ));
 
-    println!("Started serving on: {}", addr);
+    tracing::info!("Started serving on: {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service_with_connect_info::<SocketAddr>())
         .await
